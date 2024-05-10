06d3988e9c30b69f4fa153dfc7f04acf07518e19
Andreas Venetikidis <andre.venett@gmail.com>
2024-05-10 15:55:46 +0300

    Add initial dependencies

--- Weather.Precipitation/Program.cs
+++ Weather.Precipitation/Program.cs
@@ -1,5 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
+
+app.MapGet("/observation/{zipcode}", (string zipcode, [FromQuery] int? days) =>
+{
+    return Results.Ok(zipcode);
+});
 
 app.Run();
