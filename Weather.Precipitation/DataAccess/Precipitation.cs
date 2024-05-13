ffeb3f068bab006777b551853a316573bbd4996a
Andreas Venetikidis <andre.venett@gmail.com>
2024-05-13 16:39:26 +0300

    add data access for precipitation service

---
+++ Weather.Precipitation/DataAccess/Precipitation.cs
@@ -0,0 +1,10 @@
+﻿namespace Weather.Precipitation.DataAccess {
+    public class Precipitation {
+        public Guid Id { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public Decimal AmountInches { get; set; }
+        public string WeatherType { get; set; }
+        public string ZipCode { get; set; }
+
+    }
+}
--- Weather.Precipitation/Program.cs
+++ Weather.Precipitation/Program.cs
@@ -1,11 +1,27 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Weather.Precipitation.DataAccess;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<PrecipDbContext>(
+	options => {
+		options.EnableSensitiveDataLogging();
+		options.EnableDetailedErrors();
+		options.UseNpgsql(builder.Configuration.GetConnectionString("AppDb"));
+	}, ServiceLifetime.Transient
+);
+
 var app = builder.Build();
 
-app.MapGet("/observation/{zipcode}", (string zipcode, [FromQuery] int? days) =>
-{
+app.MapGet("/observation/{zipcode}", async (string zipcode, [FromQuery] int? days, PrecipDbContext db) => {
+	if (days == null || days < 1 || days < 30) {
+		return Results.BadRequest("Please provide a 'days' query param between 1 and 30");
+	}
+
+	var startDate = DateTime.UtcNow - TimeSpan.FromDays(days.Value);
+	var results = await db.Precipitation.Where(precip => precip.ZipCode == zipcode && precip.CreatedOn > startDate).ToListAsync();
+
     return Results.Ok(zipcode);
 });
 
